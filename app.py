b92da2ee0a9fb455d4bf95ecd5d175b4e4ed3ffe
mankolepanto <40011445+mankolepanto@users.noreply.github.com>
2024-08-01 23:47:07 +0200

    front

--- app.py
+++ app.py
@@ -10,17 +10,17 @@ app = Flask (__name__)
 def inicio (): # función o vista
     return render_template ('index.html')
 
+@app.route('/data')
+def data():
+    return render_template('data.html')
+
 @app.route('/modelo')
 def modelo():
     return render_template('modelo.html')
 
-@app.route('/hiperparametros')
-def hiperparametros():
-    return render_template('hiperparametros.html')
-
-@app.route('/resultados')
-def resultados():
-    return render_template('resultados.html')
+@app.route('/retrain')
+def retrain():
+    return render_template('retrain.html')
 
 if __name__ == "__main__":
     os.environ ['FLASK_ENV'] = 'development'
